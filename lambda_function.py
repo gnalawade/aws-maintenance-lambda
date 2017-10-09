4f4f545c09f47cb6eff595997959810b604a04ab
Rizki <rizki@rizkidoank.com>
2017-10-09 15:13:29 +0700

    repair indentation and reformatting

--- lambda_function.py
+++ lambda_function.py
@@ -6,6 +6,7 @@ import pprint
 from base64 import b64decode
 import boto3
 from jira import JIRA
+import jinja2
 
 # aws lambda environment variables encryption using kms
 ENCRYPTED = os.environ['JIRA_PASS']
@@ -36,36 +37,16 @@ def lambda_handler(event, context):
         os.environ['JIRA_URL'],
         basic_auth=(os.environ['JIRA_USER'], DECRYPTED))
 
-    # templating jira description
-    description = """
-    h1. Team Notes
-    h2. Blockers
-    - (-) approval from service team to execute preventive stop-start procedure
-
-    h1. Request Details
-    h2. Background
-    We have received the following alert from AWS:
-    {quote}
-    %s
-    {quote}
-
-    h2. Purpose
-    To minimize disruption to the service, we should stop and start this instance as soon as possible.
-    Stopping and starting the instance should move it to different underlying hardware and clear AWS scheduled retirement event.
-
-    h2. Impact
-    AWS notice alerts of hardware degradation, and this instance may already be impacted.
-    If you do not respond to this issue your instance will be restarted or terminated in accordance with AWS retirement schedule.
-
-    If you chose to stop-start the instance ahead of schedule, the instance will be down for approximately 5-10 minutes, but in some cases might take up to 1 hour due to hardware degradation.
-    After restart service should resume as normal (unless your application does not start automatically after reboot).
-    """ % (
-        event_description
+    template = jinja2.Environment(
+        loader=jinja2.FileSystemLoader("./")
+    ).get_template("description.j2")
+    description = template.render(
+        event_description=event_description
     )
 
     # jira issue structure
     issue_data = {
-        'summary': 'stop-start ' + ''.join(instance_ids) + ' for scheduled ' + event_type_code,
+        'summary': 'Stop-start ' + ''.join(instance_ids) + ' for scheduled ' + event_type_code,
         'project': {
             'key': os.environ['JIRA_PROJECT']},
         'description': description,
